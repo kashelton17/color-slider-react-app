a454a753d165b7d01174fdf782a4b6cc326ceeac
kashelton17 <kashelton17@gmail.com>
2021-08-14 16:22:09 -0400

    adding dynamically changing background colors

--- src/components/colorBrowser/RGB.js
+++ src/components/colorBrowser/RGB.js
@@ -1,6 +1,9 @@
 export default function RGB({ colors }) {
+    const colorStyle = {
+        backgroundColor: `rgb(${colors.red},${colors.green},${colors.blue})`,
+      };
     return (
-        <div className="rgb">
+        <div className="rgb" style={colorStyle}>
             <h3>RGB: {colors.red} {colors.green} {colors.blue}</h3>
         </div>
     )
